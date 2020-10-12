0823d4aebb1e0b95686979ca36d5e99c8511d37f
Chaim Brisk <cbrisk99@gmail.com>
2020-10-12 12:54:18 -0700

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+var fullName = 'Chaim Brisk';
+var isCool = true;
+var totalPets = 0;
+
+console.log('My Name: ', fullName);
+console.log('Cool: ', isCool);
+console.log('Total Pets: ', totalPets);
+
+console.log('Typeof Full Name: ', typeof fullName);
+console.log('Typeof Is Cool: ', typeof isCool);
+console.log('Typeof Total Pets: ', typeof totalPets);
+
+var unknown = null;
+console.log('Unknown:', unknown);
+console.log('Typeof Unknown: ', typeof unknown);
+
+var never;
+console.log('Never: ', never);
+console.log('Typeof Never: ', typeof never);
