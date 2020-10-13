81cf74af96ffef39720b40e8b90e0f55c9fdba30
Chaim Brisk <cbrisk99@gmail.com>
2020-10-12 20:22:43 -0700

    completed javascript arrays

---
+++ javascript-arrays/javascript-arrays/main.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console, no-unused-vars */
+var colors = ['red', 'white', 'blue'];
+
+console.log('Value of colors[0]: ', colors[0]);
+console.log('Value of colors[1]: ', colors[1]);
+console.log('Value of colors[2]: ', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+console.log(colors);
+
+var students = ['John', 'Mark', 'Steve', 'Bill'];
+var numberOfStudents = students.length;
+
+console.log('There are ' + numberOfStudents + ' students in the class.');
+
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+
+console.log('The last student in the array is ' + lastStudent);
+console.log('Students: ', students);
