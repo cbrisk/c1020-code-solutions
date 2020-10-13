0955aee2a01ff48b4ad4f46ff2086023f96e811a
Chaim Brisk <cbrisk99@gmail.com>
2020-10-13 11:33:15 -0700

    second correction to javascript-data-model

--- javascript-data-model/javascript-data-model/main.js
+++ javascript-data-model/javascript-data-model/main.js
@@ -2,31 +2,40 @@
 
 var orderHistory = [
   {
-    orderNo: 11439416898772232,
+    orderNo: '11439416898772232',
     dateOfOrder: 'Aug 4 2020',
     customerName: 'JS Masher',
-    items: 'JavaScript for impatient programmers',
+    items: [
+      { itemName: 'JavaScript for impatient programmers', price: 31.55, image: 'https://images-na.ssl-images-amazon.com/images/I/41jk+Ueey9L._SX348_BO1,204,203,200_.jpg' }
+    ],
     totalPrice: 34.00
   },
   {
-    orderNo: 11399842681280257,
+    orderNo: '11399842681280257',
     dateOfOrder: 'July 19 2020',
     customerName: 'JS Masher',
-    items: 'The Timeless Way of Building',
+    items: [
+      { itemName: 'The Timeless Way of Building', price: 41.33, image: 'https://images-na.ssl-images-amazon.com/images/I/41CJ2M0--6L._SX325_BO1,204,203,200_.jpg' }
+    ],
     totalPrice: 44.53
   },
   {
-    orderNo: 11428755579059409,
+    orderNo: '11428755579059409',
     dateOfOrder: 'July 7 2020',
     customerName: 'JS Masher',
-    items: 'Gamecube Controller Adapter',
+    items: [
+      { itemName: 'Gamecube Controller Adapter', price: 15.98, image: 'https://images-na.ssl-images-amazon.com/images/I/61jLJ-RXf6L._AC_SL1200_.jpg' }
+    ],
     totalPrice: 17.22
   },
   {
-    orderNo: 11328831772648248,
+    orderNo: '11328831772648248',
     dateOfOrder: 'July 5 2020',
     customerName: 'JS Masher',
-    items: ['Gamecube Controller', 'The art of SQL'],
+    items: [
+      { itemName: 'Gamecube Controller', price: 94.95, image: 'https://images-na.ssl-images-amazon.com/images/I/71Pxz0BUbbL._SL1500_.jpg' },
+      { itemName: 'The art of SQL', price: 33.99, image: 'https://images-na.ssl-images-amazon.com/images/I/51QkpAIxu5L._AC_SY400_.jpg' }
+    ],
     totalPrice: 138.93
   }
 ];
