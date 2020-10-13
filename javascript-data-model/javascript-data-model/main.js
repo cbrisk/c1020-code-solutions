1d48211ae03af8f570b6107fead60525c1785466
Chaim Brisk <cbrisk99@gmail.com>
2020-10-13 10:28:11 -0700

    completed javascript-data-model

---
+++ javascript-data-model/javascript-data-model/main.js
@@ -0,0 +1,28 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    customerName: 'John Doe',
+    city: 'Los Angeles',
+    items: 'Bose Headphones',
+    freeGift: true
+  },
+  {
+    customerName: 'Mark Black',
+    city: 'Irvine',
+    items: ['iPad', 'Laptop', 'Computer Science Book', 'Toothbrush'],
+    freeGift: false
+  },
+  {
+    customerName: 'Susan Sunnylife',
+    city: 'San Diego',
+    items: 'Pencils',
+    freeGift: false
+  },
+  {
+    customerName: 'Ted Gilson',
+    city: 'Burbank',
+    items: [{ name: 'Good Cooking', price: 29.99 }, { name: 'Fix Your Car', price: 19.99 }],
+    freeGift: true
+  }
+];
