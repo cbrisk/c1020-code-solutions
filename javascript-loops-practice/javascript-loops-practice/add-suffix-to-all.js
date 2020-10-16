e9fabf90b6ba037cae5ce81c37ec5e2731322d87
Chaim Brisk <cbrisk99@gmail.com>
2020-10-15 17:08:36 -0700

    Second fix on javascript-loops-practice

--- javascript-loops-practice/javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/javascript-loops-practice/add-suffix-to-all.js
@@ -3,7 +3,7 @@ function addSuffixToAll(words, suffix) {
   var suffixArray = [];
 
   for (var i = 0; i < words.length; i++) {
-    var word = words[i] += suffix;
+    var word = words[i] + suffix;
     suffixArray.push(word);
   }
   return suffixArray;
