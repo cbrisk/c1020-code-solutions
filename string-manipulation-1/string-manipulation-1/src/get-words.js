6914003315004a873b91ca4dc64cc4eddb586010
Chaim Brisk <cbrisk99@gmail.com>
2020-10-17 23:12:49 -0700

    completed string-manipulation-1

---
+++ string-manipulation-1/string-manipulation-1/src/get-words.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+function getWords(string) {
+  var array = [];
+  if (string !== '') {
+    array = string.split(' ');
+  }
+  return array;
+}
---
+++ string-manipulation-1/string-manipulation-1/src/is-lower-cased.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+function isLowerCased(word) {
+  var checker = word;
+  if (checker.toLowerCase() === word) {
+    return true;
+  } else {
+    return false;
+  }
+}
