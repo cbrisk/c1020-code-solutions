ccf367193dfd8809c1914f199314d4ebfd9f660a
Chaim Brisk <cbrisk99@gmail.com>
2020-10-20 11:44:47 -0700

    fixed javascript-lightbulb

--- javascript-light-bulb/javascript-light-bulb/main.js
+++ javascript-light-bulb/javascript-light-bulb/main.js
@@ -1,7 +1,14 @@
 var $container = document.querySelector('.container');
 var $circle = document.querySelector('.circle');
+var numclicks = 0;
 
 $circle.addEventListener('click', function (event) {
-  $circle.classList.add('circle-clicked');
-  $container.classList.add('container-clicked');
+  numclicks++;
+  if (numclicks % 2) {
+    $circle.classList.add('circle-clicked');
+    $container.classList.add('container-clicked');
+  } else {
+    $circle.classList.remove('circle-clicked');
+    $container.classList.remove('container-clicked');
+  }
 });
