ea2f726fbbae7ebd65420bfcf022a2c547f109b6
Chaim Brisk <cbrisk99@gmail.com>
2020-10-21 15:46:10 -0700

    completed javascript typing tutor

--- javascript-typing-tutor/javascript-typing-tutor/main.js
+++ javascript-typing-tutor/javascript-typing-tutor/main.js
@@ -2,7 +2,9 @@ var $nodeList = document.querySelectorAll('span');
 var length = $nodeList.length;
 var index = 0;
 var $div = document.querySelector('div.play');
-// var $button = document.querySelector('button');
+var $para = document.querySelector('p.score');
+var $button = document.querySelector('button');
+var wrong = 0;
 
 document.addEventListener('keydown', function (event) {
   if (event.key.trim() === $nodeList[index].textContent.trim()) {
@@ -13,8 +15,22 @@ document.addEventListener('keydown', function (event) {
     } else {
       $nodeList[index].className = 'green';
       $div.classList.remove('none');
+      $para.textContent = 'Your typing accuracy is ' + Math.floor(100 - (100 / length * wrong)) + '%';
     }
   } else {
     $nodeList[index].className = 'red';
+    wrong++;
+  }
+});
+
+$button.addEventListener('click', function (event) {
+  $para.textContent = '';
+  index = 0;
+  wrong = 0;
+  $nodeList[index].classList.add('current');
+  $div.classList.add('none');
+
+  for (var i = 0; i < length; i++) {
+    $nodeList[i].classList.remove('green');
   }
 });
