165c166bdbb447e3856f48f822d99e1231dabcda
Chaim Brisk <cbrisk99@gmail.com>
2020-10-21 14:40:42 -0700

    added some to typing

--- javascript-typing-tutor/javascript-typing-tutor/main.js
+++ javascript-typing-tutor/javascript-typing-tutor/main.js
@@ -1,12 +1,19 @@
 var $nodeList = document.querySelectorAll('span');
-// var length = $nodeList.length;
+var length = $nodeList.length;
 var index = 0;
+var $div = document.querySelector('div.play');
+// var $button = document.querySelector('button');
 
 document.addEventListener('keydown', function (event) {
-  if (event.key === $nodeList[index].textContent) {
-    index++;
-    $nodeList[index].classList.add('current');
-    $nodeList[index - 1].className = 'green';
+  if (event.key.trim() === $nodeList[index].textContent.trim()) {
+    if (index < length - 1) {
+      index++;
+      $nodeList[index].classList.add('current');
+      $nodeList[index - 1].className = 'green';
+    } else {
+      $nodeList[index].className = 'green';
+      $div.classList.remove('none');
+    }
   } else {
     $nodeList[index].className = 'red';
   }
