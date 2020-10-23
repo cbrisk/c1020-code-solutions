896f3e5fbb229f73f41da5fe7db885d02cbdb6a8
Chaim Brisk <cbrisk99@gmail.com>
2020-10-22 17:56:12 -0700

    some more array 3

--- array-manipulation-3/array-manipulation-3/src/unique.js
+++ array-manipulation-3/array-manipulation-3/src/unique.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+function unique(array) {
+  var newArray = [];
+
+  if (array.length === 0) {
+    return newArray;
+  }
+  newArray.push(array[0]);
+  for (var i = 1; i < array.length; i++) {
+    if (newArray.indexOf(array[i]) === -1) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
