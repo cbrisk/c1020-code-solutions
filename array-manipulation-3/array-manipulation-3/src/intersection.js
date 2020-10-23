4632b91468e88366a77a72faa8ee6e538cc45074
Chaim Brisk <cbrisk99@gmail.com>
2020-10-22 22:38:55 -0700

    completed array manipulation 3

--- array-manipulation-3/array-manipulation-3/src/intersection.js
+++ array-manipulation-3/array-manipulation-3/src/intersection.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function intersection(first, second) {
+  var newArray = [];
+
+  for (var i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) !== -1) {
+      newArray.push(first[i]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/array-manipulation-3/src/union.js
+++ array-manipulation-3/array-manipulation-3/src/union.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+function union(first, second) {
+  var newArray = [];
+
+  for (var i = 0; i < first.length; i++) {
+    newArray.push(first[i]);
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (newArray.indexOf(second[j]) === -1) {
+      newArray.push(second[j]);
+    }
+  }
+  return newArray;
+}
