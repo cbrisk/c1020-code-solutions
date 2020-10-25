c4b3000a1bb642bb0d1403172c9c3cc47300b5d8
Chaim Brisk <cbrisk99@gmail.com>
2020-10-25 13:43:34 -0700

    added some collections

--- working-with-collections/working-with-collections/main.js
+++ working-with-collections/working-with-collections/main.js
@@ -1,7 +1,9 @@
 /* global _ */
 /* eslint-disable no-console */
 // console.log('Lodash is loaded:', typeof _ !== 'undefined');
-
+// var person = [{ name: 'John', hand: [] }];
+// var array = [{ rank: '10' }, { rank: '10' }];
+// var pulled = _.pullAt(array, [0, 1]);
 var players = [
   { name: 'John', hand: [], points: 0 },
   { name: 'Steve', hand: [], points: 0 },
@@ -9,7 +11,6 @@ var players = [
   { name: 'Tom', hand: [], points: 0 }
 ];
 var rank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-var points = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 var suit = ['hearts', 'diamonds', 'spades', 'clubs'];
 var cards = [];
 for (var i = 0; i < suit.length; i++) {
@@ -17,24 +18,29 @@ for (var i = 0; i < suit.length; i++) {
     cards.push({ rank: rank[j], suit: suit[i] });
   }
 }
-cards = _.shuffle(cards);
-for (var k = 0; k < players; k++) {
-  players[k].hand = _.pullAt(cards, [0, 1]);
-  var card1 = players[k].hand[0];
-  var card2 = players[k].hand[1];
-  if (card1 === rank[0] && card2 === rank[9]) {
-    players[k].points += 21;
-  } else {
-    players[k].points += points[_.indexOf(rank, players.hand[0])];
-    players[k].points += points[_.indexOf(rank, players.hand[1])];
+function playGame(players, cards) {
+  var pointsVal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+  cards = _.shuffle(cards);
+
+  for (var k = 0; k < players; k++) {
+    players[k].hand.push(_.pullAt(cards, [0, 1]));
+    var card1 = players[k].hand[0].rank;
+    var card2 = players[k].hand[1].rank;
+    if (card1 === rank[0] && card2 === rank[9]) {
+      players[k].points += 21;
+    } else {
+      players[k].points += pointsVal[_.indexOf(rank, card1)];
+      players[k].points += pointsVal[_.indexOf(rank, card2)];
+    }
   }
 
-}
-for (var l = 0; l < players; l++) {
-  var winner = null;
-  var highestPoints = 0;
-  if (players.points > highestPoints) {
-    winner = players[l];
+  for (var l = 0; l < players; l++) {
+    var winner = null;
+    var highestPoints = 0;
+    if (players.points > highestPoints) {
+      winner = players[l];
+    }
+    console.log('The winner is: ', winner);
   }
-  return winner;
 }
+playGame(players, cards);
