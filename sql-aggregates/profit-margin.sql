f6358b90e627e1dac0a7227ca1d5ded86aea4628
Chaim Brisk <cbrisk99@gmail.com>
2020-12-07 15:25:17 -0800

    got the final challenge

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,9 +1,15 @@
-select "films"."title",
-           sum("payments"."amount") - "films"."replacementCost" as "profit"
-      from "films"
-      join "inventory" using ("filmId")
-      join "rentals" using ("inventoryId")
-      join "payments" using ("rentalId")
-      group by "films"."title", "films"."replacementCost"
-      order by "profit" desc
-      limit 5;
+with cte as (select "films"."title",
+            count("inventory"."filmId") * "films"."replacementCost" as "total"
+        from "films"
+        join "inventory" using ("filmId")
+        group by "films"."filmId")
+  select "films"."title",
+	     sum("payments"."amount") - "total" as "profit"
+    from "films"
+    join "inventory" using ("filmId")
+    join "rentals" using ("inventoryId")
+    join "payments" using ("rentalId")
+    join "cte" using ("title")
+group by "films"."title", "cte"."total"
+order by "profit" desc
+   limit 5;
